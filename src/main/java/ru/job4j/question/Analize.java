477ee6cab7670e205d698c25f4ffe66f8c774e1b
SweetMustard993 <bogdanasdaq@gmail.com>
2022-10-13 23:33:34 +0300

    2. Статистика по коллекции. [#45889 #350933]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,43 +1,31 @@
 package ru.job4j.question;
 
+import ru.job4j.map.Map;
+
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Analize {
     public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        HashMap<Integer, String> mapPrevious = new HashMap<>(previous.stream()
+                .collect(Collectors.toMap(User::getId, User::getName)));
+        for (User user : current) {
+            if (mapPrevious.putIfAbsent(user.getId(), user.getName()) == null) {
+                added++;
+            }
+            if (!Objects.equals(mapPrevious.putIfAbsent(user.getId(), user.getName()), user.getName())) {
+                changed++;
+            }
+            mapPrevious.remove(user.getId());
+        }
+        deleted = mapPrevious.size();
 
-        Set<Integer> idAddedPrevious = previous.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        Set<Integer> idAddedCurrent = current.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        idAddedCurrent.removeAll(idAddedPrevious);
-        int added = idAddedCurrent.size();
-
-        Set<User> changedPrevious = new HashSet<>(previous);
-        Set<User> changedCurrent = new HashSet<>(current);
-        changedCurrent.removeAll(changedPrevious);
-        Set<Integer> idRest = changedCurrent.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        Set<Integer> idPrevious = changedPrevious.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        idPrevious.retainAll(idRest);
-        int changed = idPrevious.size();
-
-        Set<Integer> idDelPrevious = previous.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        Set<Integer> idDelCurrent = current.stream()
-                .map(User::getId)
-                .collect(Collectors.toSet());
-        idDelPrevious.removeAll(idDelCurrent);
-        int deleted = idDelPrevious.size();
         return new Info(added, changed, deleted);
     }
 
