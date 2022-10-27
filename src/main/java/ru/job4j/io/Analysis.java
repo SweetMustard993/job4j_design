96331dcb31ac50f6b4f807be00ffc623af44d46e
SweetMustard993 <bogdanasdaq@gmail.com>
2022-10-27 23:48:13 +0300

    2. Анализ доступности сервера. [#859 #355369]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,30 +1,33 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Analysis {
     public void unavailable(String source, String target) {
         StringJoiner in = new StringJoiner("");
-        AtomicBoolean unavailable = new AtomicBoolean(false);
+        List<String[]> statusList = new ArrayList<>();
+        boolean unavailable = false;
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             reader.lines()
                     .map(line -> line.split(" "))
-                    .forEach(line -> {
-                        if (("400".equals(line[0]) || "500".equals(line[0])) && !unavailable.get()) {
-                            in.add(line[1]);
-                            in.add(";");
-                            unavailable.set(true);
-                        }
-                        if (("200".equals(line[0])) && unavailable.get()) {
-                            in.add(line[1]);
-                            in.add(System.lineSeparator());
-                            unavailable.set(false);
-                        }
-                    });
+                    .forEach(statusList::add);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        for (String[] status : statusList) {
+            if (!"200".equals(status[0]) && !unavailable) {
+                in.add(status[1]);
+                unavailable = true;
+            }
+            if ("200".equals(status[0]) && unavailable) {
+                in.add(status[1]);
+                in.add(System.lineSeparator());
+                unavailable = false;
+            }
         }
         try (PrintWriter out = new PrintWriter((new FileOutputStream(target)))) {
             out.print(in);
