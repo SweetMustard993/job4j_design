e0b77af3b4d0106d8dbecf8dd747f1ac924652c4
SweetMustard993 <bogdanasdaq@gmail.com>
2022-11-07 22:31:50 +0300

    4.2. Поиск дубликатов [#315066 #358456]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -8,10 +8,8 @@ import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Map<FileProperty, StringBuilder> duplicates = new HashMap<>();
-        Files.walkFileTree(Path.of("C:\\projects\\Test"), new DuplicatesVisitor(duplicates));
-        duplicates.forEach((fileProperty, stringBuilder) -> System.out.printf("%s - %s %s %s", fileProperty.getName(),
-                fileProperty.getSize(), System.lineSeparator(), stringBuilder));
-
+        DuplicatesVisitor duplicates = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:\\projects\\Test"), duplicates);
+        duplicates.printResult();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -7,32 +7,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private Map<FileProperty, StringBuilder> duplicates;
-    private Map<FileProperty, StringBuilder> files = new HashMap<>();
+    private Map<FileProperty, List<Path>> duplicates = new HashMap<>();
 
-    DuplicatesVisitor(Map<FileProperty, StringBuilder> duplicates) {
-        this.duplicates = duplicates;
-    }
-
-    public Map<FileProperty, StringBuilder> getDuplicates() {
-        return duplicates;
+    public void printResult() {
+        duplicates.forEach((fileProperty, list) -> {
+            if (list.size() >= 2) {
+                System.out.printf("%s - %s %s", fileProperty.getName(), fileProperty.getSize(), System.lineSeparator());
+                list.forEach(System.out::println);
+            }
+        });
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
-        if (files.containsKey(fileProperty)) {
-            duplicates.putIfAbsent(fileProperty, files.get(fileProperty));
-        }
-        files.putIfAbsent(fileProperty, new StringBuilder(file.toAbsolutePath().toString()));
-        duplicates.computeIfPresent(fileProperty, (k, v) -> v.append("\n").append(file.toAbsolutePath()));
+        duplicates.putIfAbsent(fileProperty, new ArrayList<Path>());
+        duplicates.get(fileProperty).add(file.toAbsolutePath());
         return super.visitFile(file, attrs);
     }
 }
