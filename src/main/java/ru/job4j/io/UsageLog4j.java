c5d52d08cf7eb7f742974511f6d05d5e9069efd7
SweetMustard993 <bogdanasdaq@gmail.com>
2023-05-10 18:03:29 +0300

    3. Slf4j - вывод переменных. [#268852 #415421]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -9,7 +9,15 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
+        String placeOfBirth = "Moscow";
+        char groupName = 'a';
         int age = 33;
-        LOG.debug("User info name : {}, age : {}", name, age);
+        boolean isStudent = true;
+        int courseGrade = 4;
+        String courseName = "information systems and networks";
+        boolean hasAcademicDebts = false;
+        LOG.debug("User info name : {}, place og birth: {}, age : {}, student: {}, grade: {}, course name: {}, "
+                        + "groupe name: {}, hasAcademicDebts: {}", name, placeOfBirth, age, isStudent,
+                courseGrade, courseName, groupName, hasAcademicDebts);
     }
 }
