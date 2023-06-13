f13eaf14dcf5f4b955e461920dc7553295c430be
SweetMustard993 <bogdanasdaq@gmail.com>
2023-06-13 15:32:21 +0300

    1. Связи между таблицами [#373615]

--- src/main/java/ru/job4j/sql/script2.sql
+++ src/main/java/ru/job4j/sql/script2.sql
@@ -12,22 +12,33 @@ country text,
 telephone text references buyer(telephone)
 );
 
-create table order (
+create table orders (
 id serial primary key,
 name varchar(255),
 telephone text references buyer(telepone)
-);
-
-
-create table order_list (
-order_id int references order (id) not null,
-goods_id int references goods (id) not null,
-count int,
-primary key (order_id, goods_id)
 );
 
 create table goods (
 id serial ptimay key,
 name varchar(255),
 price decimal(10,2)
+);
+
+create table order_list (
+order_id int references orders (id) not null,
+goods_id int references goods (id) not null,
+count int,
+primary key (order_id, goods_id)
+);
+
+
+create table drivers (
+telephone text primary key,
+name varchar(255),
+car_id int references company_car (car_id) unique
+);
+
+create table company_car (
+car_id primary key,
+model text
 );
