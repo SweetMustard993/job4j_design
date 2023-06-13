c8d66ce1398e63138282212069ff53661e2fe30e
SweetMustard993 <bogdanasdaq@gmail.com>
2023-06-13 15:17:51 +0300

    0. Что такое база данных. [#6860 #422679]

---
+++ src/main/java/ru/job4j/sql/script2.sql
@@ -0,0 +1,32 @@
+create table buyer (
+telephone text primary key,
+name varchar(255)
+);
+
+create table adress (
+id serial primary key,
+city text,
+street text,
+postal code text,
+country text,
+telephone text references buyer(telephone)
+);
+
+create table order (
+id serial primary key,
+name varchar(255),
+telephone text references buyer(telepone)
+);
+
+create table goods (
+id serial ptimay key,
+name varchar(255),
+price decimal(10,2)
+);
+
+create table order_list (
+order_id int references order (id) not null,
+goods_id int references goods (id) not null,
+count int,
+primary key (order_id, goods_id)
+);
