334726901535ab2f7a9a54dd500c7cd5877c43b6
SweetMustard993 <bogdanasdaq@gmail.com>
2023-06-27 18:51:48 +0300

    2. Триггеры [#504803]

---
+++ src/main/java/ru/job4j/sql/scipt10.sql
@@ -0,0 +1,68 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price * 1.2
+        where id = (select id from inserted);
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure tax();
+
+
+create or replace function tax2()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price * 1.2
+        where id = new.id;
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger2
+    before insert on products
+    for each row
+    execute procedure tax2();
+
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+
+create or replace function save()
+	returns trigger as
+$$
+	begin
+		insert into history_of_price(name,price,date) values (new.name, new.price, current_date);
+		return new;
+	end;
+$$
+language 'plpgsql';
+
+create trigger history_save
+	after insert on history_of_price
+	for each row
+	execute procedure save();
+
